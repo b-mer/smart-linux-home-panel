fd7bb4819a657db31c4ca6747850a02e393f7d3d
b-mer <163482111+b-mer@users.noreply.github.com>
2025-04-22 12:46:25 +1000

    Setting up web app.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+const public_path = path.join(__dirname, 'public')
+
+app.get('/', (req, res) => {
+    res.send("testing");
+})
+
+app.use(express.static(public_path));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
