9e106d2de445d726ff7f7c2272e8b0121de2f318
b-mer <163482111+b-mer@users.noreply.github.com>
2025-05-02 13:01:08 +1000

    Changed the way the side panel behaves.

--- src/public/global.js
+++ src/public/global.js
@@ -1,27 +1,4 @@
 // Code to run on webpage load.
-window.onload = function() {
-    // Function to close the main panel.
-    function close_main_panel() {
-        document.getElementById("main_panel").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-    };
+// window.onload = function() {
 
-    // Function to open the main panel.
-    function open_main_panel() {
-        document.getElementById("main_panel").style.width = "20vw";
-        document.getElementById("main").style.marginLeft = "20vw";
-    };
-
-
-    // Function to toggle the main panel.
-    function toggle_main_panel() {
-        if (document.getElementById("main_panel").style.width > "10px") {
-            close_main_panel()
-        } else {
-            open_main_panel()
-        }
-    }
-
-    // Add click effect to main panel toggle button.
-    document.getElementById("panel_button").addEventListener("click", toggle_main_panel)
-}    
+// }    
