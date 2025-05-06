2927ae3d5e77a0e91213bb7e0223289a4658952b
b-mer <163482111+b-mer@users.noreply.github.com>
2025-05-06 12:50:04 +1000

    Added work in progress trash can for widgets

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const public_path = path.join(__dirname, 'public')
+const public_path = path.join(__dirname, '../public')
 
 app.use(express.static(public_path));
 
